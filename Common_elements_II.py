a5fc9c801adcc122f603c1def1d0550e4ec54ec7
Amrutha <97725227+DADALAAMRUTHA@users.noreply.github.com>
2022-09-12 21:25:25 +0530

    n,m=map(int,input().split()) l=list(map(int,input().split())) ll=list(map(int,input().split())) a=[] for i in l:     if i not in ll:         a.append(i) for i in ll:     if i not in l:         a.append(i) print(*a)

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -1,10 +1,11 @@
-a,b=map(int,input().split())
-l1=list(map(int,input().split()))
-l2=list(map(int,input().split()))
-d=[]
-for i in l1:
-    if i not in l2:
-        print(i,end=" ")
-for i in l2:
-    if i not in l1:
-        print(i,end=" ") 
+n,m=map(int,input().split())
+l=list(map(int,input().split()))
+ll=list(map(int,input().split()))
+a=[]
+for i in l:
+    if i not in ll:
+        a.append(i)
+for i in ll:
+    if i not in l:
+        a.append(i)
+print(*a)
