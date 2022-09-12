375d36aff11628f17a052ffc5482a5fbcb53c323
Amrutha <97725227+DADALAAMRUTHA@users.noreply.github.com>
2022-09-12 14:48:40 +0530

    n=int(input()) l=list(map(str,input().split())) a=[] for i in l:     a.append(len(i)) print(a.count(max(a)))

--- Max_digits_count.py
+++ Max_digits_count.py
@@ -1,11 +1,6 @@
 n=int(input())
-arr=list(map(int,input().split()))
-c=0
-max=0
-for i in range(n):
-    if len(str(arr[i]))>max:
-        max=len(str(arr[i]))
-for i in range(n):
-    if len(str(arr[i]))==max:
-        c=c+1
-print(c)
+l=list(map(str,input().split()))
+a=[]
+for i in l:
+    a.append(len(i))
+print(a.count(max(a)))
