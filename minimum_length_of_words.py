64884f1c750945f57b24f7323cade0a8a36c6e17
Amrutha <97725227+DADALAAMRUTHA@users.noreply.github.com>
2022-10-23 23:14:31 +0530

    l=list(map(str,input().split())) min=999 for i in range(len(l)):     if len(l[i])<min:         min=len(l[i]) print(min)

--- minimum_length_of_words.py
+++ minimum_length_of_words.py
@@ -1,9 +1,6 @@
-n=input()
-s=n.split()
-c=0
-min=1000
-for i in s:
-    c=len(i)
-    if min>c:
-        min=c
-print(min) 
+l=list(map(str,input().split()))
+min=999
+for i in range(len(l)):
+    if len(l[i])<min:
+        min=len(l[i])
+print(min)
