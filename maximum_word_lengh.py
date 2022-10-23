8ee3bca8a49b465815d6500c92a96cbc3946e02c
Amrutha <97725227+DADALAAMRUTHA@users.noreply.github.com>
2022-10-23 23:12:10 +0530

    l=list(map(str,input().split())) min=0 for i in range(len(l)):     if len(l[i])>min:         min=len(l[i]) print(min)

--- maximum_word_lengh.py
+++ maximum_word_lengh.py
@@ -1,9 +1,6 @@
-n=input()
-s=n.split()
-max=0
-c=0
-for i in s:
-    c=len(i)
-    if max<c:
-        max=c
-print(max) 
+l=list(map(str,input().split()))
+min=0
+for i in range(len(l)):
+    if len(l[i])>min:
+        min=len(l[i])
+print(min)
