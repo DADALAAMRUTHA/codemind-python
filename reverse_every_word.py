1066e389b9053603e5a0a0ddc59f3fa789a4e2d0
Amrutha <97725227+DADALAAMRUTHA@users.noreply.github.com>
2022-10-23 23:20:47 +0530

    l=list(map(str,input().split())) for i in range(len(l)):     l[i]=l[i][::-1] print(*l)

--- reverse_every_word.py
+++ reverse_every_word.py
@@ -1,10 +1,4 @@
-a=str(input())
-b=a.split()
-c=[]
-d=[]
-for i in range(len(b)):
-    c.append(b[i])
-for j in range(len(c)):
-    m=c[j][::-1]
-    d.append(m)
-print(*d)
+l=list(map(str,input().split()))
+for i in range(len(l)):
+    l[i]=l[i][::-1]
+print(*l)
