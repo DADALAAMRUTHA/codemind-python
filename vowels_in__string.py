0aaaf32cd0369d100534dae7a292e315c2c09461
Amrutha <97725227+DADALAAMRUTHA@users.noreply.github.com>
2022-10-23 23:35:09 +0530

    s="AEIOUaeiou" n=input() ss=[] for i in n:     if i in s:         if i not in ss:             ss.append(i) print(*ss)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,11 +1,8 @@
+s="AEIOUaeiou"
 n=input()
-k='aeiouAEIOU'
-d=[]
-p=0
+ss=[]
 for i in n:
-    if i in k and i not in d:
-        d.append(i)
-if len(d)>0:
-    print(*d)
-else:
-    print(-1)
+    if i in s:
+        if i not in ss:
+            ss.append(i)
+print(*ss)
