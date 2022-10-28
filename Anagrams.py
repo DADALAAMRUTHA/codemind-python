d503b7e7f1e5890de0d778334ed3dd57fa4828d0
Amrutha <97725227+DADALAAMRUTHA@users.noreply.github.com>
2022-10-28 18:48:43 +0530

    n=input() n1=input()
    n=set(n.lower())
    n1=set(n1.lower())
    if n==n1:
        print(True)
    else:
        print(False)

--- Anagrams.py
+++ Anagrams.py
@@ -1,12 +1,9 @@
+n=input()
 n1=input()
-n2=input()
-n3=n1.lower()
-n4=n2.lower()
-c=0
-for i in n3:
-    if i in n4:
-        c=c+1
-if c==len(n3)==len(n4):
-    print('True')
+
+n=set(n.lower())
+n1=set(n1.lower())
+if n==n1:
+    print(True)
 else:
-    print('False')
+    print(False)
